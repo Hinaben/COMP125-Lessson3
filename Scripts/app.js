4c840105c058a34dee256cd1675aa5ce77430ddd
Hinaben <henapatel21@hotmail.com>
2016-05-27 23:24:10 -0400

    added about and project pages

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,31 +2,27 @@
 // IIFE - Immediately Invoked Function Expression
 (function(){
     "use strict";
-    
-    // define an array of HTML elements
-    var paragraphElements = [];
-    
-    paragraphElements[0] = document.getElementById("paragraphOne");
-    paragraphElements[1] = document.getElementById("paragraphTwo");
-    paragraphElements[2] = document.getElementById("paragraphThree");
-    
-    // define your paragraphs array;
-    var paragraphs = [];
-    
-    // data for my pages
-    paragraphs[0] = "This is my first paragraph. It is only visible on the first page. This next sentence is to prove that this really works!!!";
-    paragraphs[1] = "This is my second paragraph. It is only visible on the second page";
-    paragraphs[2] = "This is my third paragraph. It is only visible on the third page";
-    
-    // second way to define an array
-    //var paragraphs = new Array();
-    
-    // check to see if paragraph one exists
-    for (var index = 0; index < paragraphElements.length; index++) {
-        if(paragraphElements[index]) {
-         paragraphElements[index].textContent = paragraphs[index];
-        }
+  
+  //define an array of HTML elements
+  var paragraphElements = [];
+  
+  paragraphElements[0]= document.getElementById("paragraphOne");  
+  paragraphElements[1]= document.getElementById("paragraphTwo");  
+  paragraphElements[2]= document.getElementById("paragraphThree");    
+  
+  //define your paragraphs array
+  var paragraphs = [];
+  
+  //
+  paragraphs[0] = "This page is my home page. The best is yet to come!"
+  paragraphs[1] = "This is my project page and project details will be added later."
+  paragraphs[2] = "My name is Hina Patel and my student number is 821021425."
+   
+    //check to see if paragraph one exists
+  for (var index = 0; index < paragraphElements.length; index++){
+    if(paragraphElements[index]){
+      paragraphElements[index].textContent = paragraphs[index];
     }
-    
-    
+  }
+  
 })();
